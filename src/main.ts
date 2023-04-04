3ca53a4a54185475abb1637b5711e4ddb04f95d9
abdulazizv <abdulazizheir@gmail.com>
2023-04-04 16:33:28 +0500

    added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const port = process.env.PORT || 7777;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Server running at ${port}`);
+  });
 }
 bootstrap();
